704ce14322ecb57f83e3bcae3be034b264155874
Kai <kai.moritamcvey@gmail.com>
2018-09-13 21:13:22 -0700

    Removed useless setting options, added better comments

--- options.js
+++ options.js
@@ -1,32 +1,29 @@
 // Saves options to chrome.storage
 function save_options() {
-    var file = document.getElementById('file').value;
-    var likesColor = document.getElementById('like').checked;
-    chrome.storage.sync.set({
-      favoriteType: file,
-      likesColor: likesColor
-    }, function() {
-      // Update status to let user know options were saved.
-      var status = document.getElementById('status');
-      status.textContent = 'Options saved.';
-      setTimeout(function() {
-        status.textContent = '';
-      }, 750);
-    });
-  }
-  
-  // Restores select box and checkbox state using the preferences
-  // stored in chrome.storage.
-  function restore_options() {
-    // Use default value file = 'jpg' and likesColor = true.
-    chrome.storage.sync.get({
-      favoriteType: 'jpg',
-      likesColor: true
-    }, function(items) {
-      document.getElementById('file').value = items.favoriteType;
-      document.getElementById('like').checked = items.likesColor;
-    });
-  }
-  document.addEventListener('DOMContentLoaded', restore_options);
-  document.getElementById('save').addEventListener('click',
-      save_options);    
+  //gets file type from list on "options.html" and saves it with the chrome.storage api
+  var file = document.getElementById('file').value;
+  chrome.storage.sync.set({
+    favoriteType: file,
+  }, function() {
+    // Update status to let user know options were saved.
+    var status = document.getElementById('status');
+    status.textContent = 'Options saved.';
+    setTimeout(function() {
+      status.textContent = '';
+    }, 750);
+  });
+}
+
+// Restores select box state using the preferences
+// stored in chrome.storage.
+function restore_options() {
+  // Use default value file = 'jpg'
+  chrome.storage.sync.get({
+    favoriteType: 'jpg',
+  }, function(items) {
+    document.getElementById('file').value = items.favoriteType;
+  });
+}
+document.addEventListener('DOMContentLoaded', restore_options);
+document.getElementById('save').addEventListener('click',
+    save_options);    
